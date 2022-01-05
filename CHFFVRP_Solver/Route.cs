628a483f267cbeef1a0629f2e5000bed2a0379b0
schrob <robin.scheid@orbis.de>
2022-01-05 14:08:00 +0100

    Initial

--- CHFFVRP_Solver/Route.cs
+++ CHFFVRP_Solver/Route.cs
@@ -9,5 +9,41 @@ namespace CHFFVRP_Solver
     class Route
     {
         public List<Node> nodes;
+        public double distance; // total distance traveled on this route
+        public double emission; // total emissions by vehicle serving this route
+        public Vehicle vehicle;
+        
+        public Route(List<Node> nodes)
+        {
+            this.nodes = nodes;
+            this.distance = GetTotalDistance();
+            this.emission = GetTotalEmission();
+        }
+
+        public double GetTotalDistance()
+        {
+            this.distance = GetTotalDistance(this.nodes);
+            return this.distance;
+        }
+
+        public double GetTotalDistance(List<Node> r)
+        {
+            double distance = 0;
+            for(int i = 1; i < r.Count; i++)
+            {
+                distance += GetDistance(r[i - 1], r[i]);
+            }
+            return distance;
+        }
+
+        public double GetDistance(Node a, Node b)
+        {
+            return Math.Sqrt(Math.Pow(a.x - b.x, 2) + (Math.Pow(a.y - b.y, 2)));
+        }
+
+        public double GetTotalEmission()
+        {
+            return this.distance * this.vehicle.emissions;
+        }
     }
 }
